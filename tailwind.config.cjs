6701ef1c5cbe0ac64676355a5692287dde679425
Kate <knaychuk@gmail.com>
2023-04-29 22:29:34 -0700

    navbar template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,8 @@ module.exports = {
 				poppins: ["Poppins", "sans-serif"]
 			},
 			boxShadow: {
-				"custom": "0 12px 20px rgba(0,0,0,.35)"
+				"project-cards": "0 12px 20px rgba(0,0,0,.35)",
+				"navbar": "box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px"
 			},
 		},
 	},
